470b8226875078417498825c30584febe9196461
gfj0814 <1805648841@qq.com>
2018-07-23 21:39:53 +0800

    添加一个类

---
+++ src/gitstudy/StududyGitOne.java
@@ -0,0 +1,14 @@
+package gitstudy;
+
+/**
+ * Created by gfj43958 on 2018/7/23.
+ * time:21:37
+ * author:GuoFangjun
+ */
+public class StududyGitOne {
+
+    public static void main(String[] args) {
+        System.out.println("我就是核心的方法");
+    }
+    
+}
